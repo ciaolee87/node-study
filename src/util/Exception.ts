19bc8c5e8a7f3f0f75fc217de883e25366e28d5c
JunHyeok Lee <jh.lee@bitor-group.com>
2019-01-16 15:28:22 +0900

    9.1 초기화

--- src/util/Exception.ts
+++
@@ -1,8 +0,0 @@
-import createHttpError from "http-errors";
-
-export class Exception {
-    constructor(errCode: string, args?: any) {
-
-        createHttpError(errCode);
-    }
-}
--- src/views/sequelize.js
+++
@@ -1,6 +0,0 @@
-[].forEach.call(document.querySelectorAll('#user-list tr'), function (el) {
-   el.addEventListener('click', function () {
-       var id = el.querySelector('td').textContent;
-       getCommnet(id);
-   });
-});
